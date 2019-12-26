7e0787bd5a73e09d57da36bed84808db088515d9
Caio Almeida <caioalmeidanaweb@gmail.com>
2019-12-25 23:13:53 -0400

    avatar problem

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
+
+import AvatarInput from './AvatarInput';
+
+export default function Profile() {
+    const profile = useSelector(state => state.user.profile);
+    const dispatch = useDispatch();
+
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <AvatarInput name="avatar_id" />
+                <Input name="name" placeholder="Nome Completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu e-mail aqui"
+                />
+                <hr />
+                <Input
+                    name="oldPassword"
+                    type="password"
+                    placeholder="Sua senha atual"
+                />
+                <Input
+                    name="newPassword"
+                    type="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    name="confirmPassword"
+                    type="password"
+                    placeholder="Confirme a nova senha"
+                />
+
+                <button type="submit">Atualizar Perfil</button>
+            </Form>
+
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
+}
